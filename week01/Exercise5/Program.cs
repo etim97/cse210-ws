ec2afddd317c6a6c2512dff606f2a6ac96bd2d14
Etim 97 <Wisdomlds97@gmail.com>
2025-01-15 23:19:32 +0100

    New code

--- week01/Exercise5/Program.cs
+++ week01/Exercise5/Program.cs
@@ -4,6 +4,21 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise5 Project.");
+      Console.WriteLine("Welcome to the program!");
+
+        Console.Write("Please enter your name: ");
+        string name = Console.ReadLine();
+
+        Console.Write("Please enter your favorite number: ");
+        int favoriteNumber = int.Parse(Console.ReadLine());
+
+        DisplaySquareMessage(name, favoriteNumber);
     }
-}
+
+    static void DisplaySquareMessage(string name, int number)
+    {
+        int square = number * number;
+        Console.WriteLine($"{name}, the square of your number is {square}");
+    }
+}  
+    
