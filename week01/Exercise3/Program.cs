0f8c63f84545135678db86d2521398c70ca7a64d
Etim 97 <Wisdomlds97@gmail.com>
2025-01-15 23:25:02 +0100

    new

--- week01/Exercise3/Program.cs
+++ week01/Exercise3/Program.cs
@@ -3,7 +3,34 @@ using System;
 class Program
 {
     static void Main(string[] args)
+     
     {
-        Console.WriteLine("Hello World! This is the Exercise3 Project.");
+    
+        int magicNumber = 18; 
+        int guess;
+
+        Console.WriteLine("What is the magic number? " + magicNumber);
+
+        do
+        {
+            Console.Write("What is your guess? ");
+            guess = int.Parse(Console.ReadLine());
+
+            if (guess < magicNumber)
+            {
+                Console.WriteLine("Higher");
+            }
+            else if (guess > magicNumber)
+            {
+                Console.WriteLine("Lower");
+            }
+            else
+            {
+                Console.WriteLine("You guessed it!");
+            }
+        } while (guess != magicNumber);
     }
-}
+} 
+
+        
+    
--- week01/Exercise4/Program.cs
+++ week01/Exercise4/Program.cs
@@ -4,6 +4,47 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello World! This is the Exercise4 Project.");
+        List<int> numbers = new List<int>();
+        int input;
+
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+
+        do
+        {
+            Console.Write("Enter number: ");
+            input = int.Parse(Console.ReadLine());
+
+            if (input != 0)
+            {
+                numbers.Add(input);
+            }
+        } while (input != 0);
+
+        if (numbers.Count > 0)
+        {
+            int sum = 0;
+            int largest = numbers[0];
+
+            foreach (int num in numbers)
+            {
+                sum += num;
+                if (num > largest)
+                {
+                    largest = num;
+                }
+            }
+
+            double average = (double)sum / numbers.Count;
+
+            Console.WriteLine($"The sum is: {sum}");
+            Console.WriteLine($"The average is: {average}");
+            Console.WriteLine($"The largest number is: {largest}");
+        }
+        else
+        {
+            Console.WriteLine("No numbers were entered.");
+        }
     }
 }
+
+    
